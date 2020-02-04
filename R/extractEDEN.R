a60947901acfd7f6e576c51862f4e386375497b8
Troy Hill <Hill.Troy@gmail.com>
2020-02-04 11:33:03 -0500

    updated EVER4CAST post-processing. Added function to pull EDEN data for an area of interest, with output matching tracePlot() inputs.

--- R/extractEDEN.R
+++ R/extractEDEN.R
@@ -1,6 +1,6 @@
-#' extractSimData
+#' getEDENbyROI
 #'
-#' @description This function extracts data and aggregates Ever4Cast simulation data (contained in a list of netCDFs) for spatial locations of interest. The output is a list of four permutations of the same data. This function can take a very long time depending on the number of regions-of-interest and their size (e.g., 2 hours for ~50 Combined Operations Plan indicator regions). NOTE: This is based on availability of data on the home page of EDEN. If data aren't there, they aren't accessible for this code.
+#' @description This function extracts EDEN data for spatial locations of interest. This function can take a very long time depending on the number of regions-of-interest and their size, and if EDEN data is downloaded within the function call. NOTE: This is based on availability of data on the home page of EDEN. If data aren't there, they aren't accessible for this code.
 #' 
 #' @param targetLocations      locations to extract data from; must be class SpatialPointsDataFrame or SpatialPolygonsDataFrame
 #' @param targetLocationNames  option to specify the name of target locations (e.g., pts$gage)
@@ -31,24 +31,40 @@ getEDENbyROI <- function(targetLocations,
                          dateRange,
                          needEDEN = TRUE # either TRUE, or the name of the list of EDEN data generated from lapply(dateRange, fireHydro::getEDEN). 
 ) {
-  if (typeof(needEDEN) == logical && needEDEN) {
+  if (length(needEDEN) == 1 && typeof(needEDEN) == logical && needEDEN) {
     test <- lapply(dateRange, fireHydro::getEDEN)
   } else if(is.list(needEDEN)) {
     test <- needEDEN
   } else {
     stop("needEDEN argument not recognized. It should be either 'TRUE' (signaling that EDEN data should be pulled) or a list of EDEN data.")
   }
-  
-  IR.means <- sapply(test, function(x) {
-    x.sf  <- x$data
-    a.sub <- sf::st_intersection(x.sf, sf::st_set_crs(sf::st_as_sf(targetLocations), sf::st_crs(x.sf)))
-    mean(a.sub$WaterDepth, na.rm = TRUE)
-  })
-  # beepr::beep()
-  dat <- data.frame(cluster  = "observed",
-                    name     = targetLocationNames,
-                    date     = as.Date(as.character(sapply(test, "[[", 1)), format = "%Y%m%d"),
-                    ave      = IR.means, 
-                    stdev    = 0)
+  for (i in 1:length(targetLocations)) {
+    IR.tmp <- sapply(test, function(x) {
+      x.sf  <- x$data
+      a.sub <- sf::st_intersection(x.sf, sf::st_set_crs(sf::st_as_sf(targetLocations[i, ]), sf::st_crs(x.sf)))
+      mean(a.sub$WaterDepth, na.rm = TRUE)
+    })
+    dat.tmp <- data.frame(cluster  = "observed",
+                          name     = targetLocationNames[i],
+                          date     = as.Date(as.character(sapply(test, "[[", 1)), format = "%Y%m%d"),
+                          ave      = IR.tmp, 
+                          stdev    = 0)
+    if (i == 1) {
+      dat <- dat.tmp
+    } else {
+      dat <- rbind(dat, dat.tmp)
+    }
+  }
+  # IR.means <- sapply(test, function(x) {
+  #   x.sf  <- x$data
+  #   a.sub <- sf::st_intersection(x.sf, sf::st_set_crs(sf::st_as_sf(targetLocations), sf::st_crs(x.sf)))
+  #   mean(a.sub$WaterDepth, na.rm = TRUE)
+  # })
+  # # beepr::beep()
+  # dat <- data.frame(cluster  = "observed",
+  #                   name     = targetLocationNames,
+  #                   date     = as.Date(as.character(sapply(test, "[[", 1)), format = "%Y%m%d"),
+  #                   ave      = IR.means, 
+  #                   stdev    = 0)
   invisible(dat)
 }

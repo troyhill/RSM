e8bdf42e598925dccf0e68ca6aa5c34a851b906b
troyhill <hill.troy@gmail.com>
2022-03-09 15:48:55 -0500

    sf/terra conversion

--- R/getFeatureIDs.R
+++ R/getFeatureIDs.R
@@ -1,18 +1,20 @@
 #' getFeatureIDs
 #'
-#' @description Extracts cell IDs from a model mesh for each indicator region. Currently only supports spatialPolygonDataframes; spatialLines and spatialPoints have not been tested.
+#' @description Extracts cell IDs from a model mesh for each indicator region. Currently only supports sf objects
 #' 
 #'  
-#' @param  shpFeature       a spatial*Dataframe for the indicator region, transect, point
-#' @param  mesh             shapefile with model mesh cells and cell ID data (mesh must have a column named "CellId")
+#' @param  shpFeature       an sf object with the indicator region, transect, point
+#' @param  mesh             sf object with model mesh cells and cell ID data (mesh must have a column named "CellId")
 #' @param  cellIDcolumn     name of column containing cell identifiers. Case insensitive.
+#' @param returnType        class of spatial data returned. Can be 'sf' or 'spatVector' (default is the latter)
 #'
-#' @return a list with two elements: a vector of cell IDs, and (2) a spatial dataframe with the intersection of the mesh and the input shapefile
+#' @return a list with two elements: a vector of cell IDs, and (2) a spatial object (sf or spatVector) with the intersection of the mesh and the input shapefile
 #'
-#' @importFrom sp     spTransform
-#' @importFrom raster intersect
-#' @importFrom raster crs
-#' @importFrom rgdal  readOGR
+#' @importFrom sf     st_transform
+#' @importFrom sf     st_crs
+#' @importFrom terra  crop
+#' @importFrom terra  vect
+#' @importFrom sf     st_as_sf
 #' 
 #' @export
 #' 
@@ -23,18 +25,45 @@
 # Sys.setenv(PATH = paste(old_path, "C:\\Rtools\\bin;C:\\Rtools\\gcc-4.6.3\\bin;C:\\Rtools\\perl\\bin;C:\\Rtools\\MinGW\\bin", sep = ";"))
 
 
-getFeatureIDs <- function(shpFeature, # = as(fireHydro::BICY_EVER_PlanningUnits,  "Spatial"), # test that this works on points, lines, polygons. MUST BE A spatial*DF  
-                          mesh = rgdal::readOGR(system.file("extdata/gis/nsrsm_v352", "nsrsm_mesh_landuse.shp", package="RSM"),"nsrsm_mesh_landuse"),# mesh must have a column named "CellId"
-                          cellIDcolumn = "objectid") { 
-  ### make CRS of shpFeature == mesh
-  shpFeature <- sp::spTransform(shpFeature, crs(mesh))
+getFeatureIDs <- function(shpFeature, # = fireHydro::BICY_EVER_PlanningUnits, # test that this works on points, lines, polygons. MUST BE A spatial*DF  
+                          mesh = sf::read_sf(system.file("extdata/gis/nsrsm_v352", "nsrsm_mesh_landuse.shp", package="RSM"),"nsrsm_mesh_landuse"),# mesh must have a column named "CellId"
+                          cellIDcolumn = "CellId",
+                          returnType = 'sf') { 
+  # ### make CRS of shpFeature == mesh
+  # shpFeature <- as(fireHydro::BICY_EVER_PlanningUnits, 'Spatial')
+  # mesh <- readOGR(system.file("extdata/gis/nsrsm_v352", "nsrsm_mesh_landuse.shp", package="RSM"),"nsrsm_mesh_landuse")
+  # shpFeature <- sp::spTransform(shpFeature, crs(mesh))
+  # ### clip
+  # int <- raster::intersect(x = mesh,  y = shpFeature) 
+  # ### pull and return cellIDs
+  # plot(int)
+  # getName <- grep(x = toupper(names(int)),   pattern = toupper(cellIDcolumn))  
+  # returnIDs <- as.data.frame(int[, getName]@data)
+  # invisible(list(cellIDs = as.numeric(returnIDs[, 1]), spdf = int[, getName]))
+  
+  
+  ##### new version
+  # options(sf_max.plot=1)
+  # shpFeature <- fireHydro::BICY_EVER_PlanningUnits
+  # mesh <- sf::read_sf(system.file("extdata/gis/nsrsm_v352", "nsrsm_mesh_landuse.shp", package="RSM"),"nsrsm_mesh_landuse")
+  
+  shpFeature <- sf::st_transform(x = shpFeature, crs = sf::st_crs(mesh))
+  
+  # plot(st_geometry(shpFeature))
+  # plot(mesh, add = TRUE)
+  # plot(st_geometry(shpFeature), add = TRUE)
   ### clip
-  int <- raster::intersect(x = mesh,  y = shpFeature) 
+  int <- terra::crop(x = terra::vect(mesh),  y = terra::vect(shpFeature)) 
   ### pull and return cellIDs
   plot(int)
-  getName <- grep(x = toupper(names(int)),   pattern = toupper(cellIDcolumn))  
-  returnIDs <- as.data.frame(int[, getName]@data)
-  invisible(list(cellIDs = as.numeric(returnIDs[, 1]), spdf = int[, getName]))
+  getName <- grep(x = toupper(names(int)),   pattern = toupper(cellIDcolumn)) 
+  returnIDs <- as.data.frame(int[, getName])
+  
+  if (returnType = 'sf') { 
+    int <- sf::st_as_sf(int)
+  }
+  invisible(list(cellIDs = as.numeric(returnIDs[, 1]), mesh = int[, getName]))
+  
 }
 
 
